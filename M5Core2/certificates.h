90e81ab3cfad308cf59f52968ffdabdfef6d6df3
Aniket Dhole <40766879+aniketdhole07@users.noreply.github.com>
2021-09-19 18:54:09 +0530

    Create certificates.h

---
+++ M5Core2/certificates.h
@@ -0,0 +1,17 @@
+include <pgmspace.h>
+
+#define SECRET
+
+const char ssid[] = "";
+const char pass[] = "";
+
+#define THINGNAME ""
+const char MQTT_HOST[] = "";
+static const char cacert[] PROGMEM = R"EOF(
+)EOF";
+
+static const char client_cert[] PROGMEM = R"KEY(
+)KEY";
+
+static const char privkey[] PROGMEM = R"KEY(
+)KEY";
